fa666fe73c88b264e449427c5488fb902d6692c5
Alec Barton <Abartondeveloper@gmail.com>
2020-04-23 13:19:53 -0400

    Initial Commit

---
+++ WorkoutLog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorkoutLog
+//
+//  Created by Alec Barton on 4/23/20.
+//  Copyright © 2020 Alec Barton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
