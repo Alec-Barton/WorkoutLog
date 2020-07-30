fd9c5a30ae5bd6b3cc44d7eccc9daee87968383b
BartonDev <abartondeveloper@gmail.com>
2020-07-29 20:11:48 -0400

    Refactor color theme

---
+++ WorkoutLog/ColorTheme.swift
@@ -0,0 +1,19 @@
+//
+//  ColorTheme.swift
+//  WorkoutLog
+//
+//  Created by Alec Barton on 7/29/20.
+//  Copyright © 2020 Alec Barton. All rights reserved.
+//
+
+import UIKit
+
+//TODO: Refactor this
+struct ColorTheme {
+//  static var lightGray: UIColor  { return UIColor(hex: "#f2f1f6") }
+    static var lightGray1: UIColor  { return UIColor(hex: "#dadada") }
+    static var lightGray2: UIColor  { return UIColor(hex: "#cdcdcd") }
+    static var lightGray3: UIColor  { return UIColor(hex: "#c0c0c0") }
+    static var lightGray4: UIColor  { return UIColor(hex: "#b4b4b4") }
+    static var lightGray5: UIColor  { return UIColor(hex: "#a7a7a7") }
+}
