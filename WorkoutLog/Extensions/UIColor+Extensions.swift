0f2a329adacade705c132b46818c810488596161
BartonDev <abartondeveloper@gmail.com>
2020-07-31 22:01:58 -0400

    Add timer class

--- WorkoutLog/Extensions/UIColor+Extensions.swift
+++ WorkoutLog/Extensions/UIColor+Extensions.swift
@@ -7,16 +7,6 @@
 //
 import UIKit
 
-//TODO: Refactor this
-struct ColorTheme {
-//  static var lightGray: UIColor  { return UIColor(hex: "#f2f1f6") }
-    static var lightGray1: UIColor  { return UIColor(hex: "#dadada") }
-    static var lightGray2: UIColor  { return UIColor(hex: "#cdcdcd") }
-    static var lightGray3: UIColor  { return UIColor(hex: "#c0c0c0") }
-    static var lightGray4: UIColor  { return UIColor(hex: "#b4b4b4") }
-    static var lightGray5: UIColor  { return UIColor(hex: "#a7a7a7") }
-}
-
 extension UIColor {
     //TODO: Improve this
     public convenience init(hex: String) {
---
+++ WorkoutLog/Model/Timer.swift
@@ -0,0 +1,13 @@
+//
+//  Timer.swift
+//  WorkoutLog
+//
+//  Created by Alec Barton on 7/31/20.
+//  Copyright © 2020 Alec Barton. All rights reserved.
+//
+
+import Foundation
+
+class Timer {
+    
+}
