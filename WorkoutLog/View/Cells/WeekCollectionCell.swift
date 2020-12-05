e0e96a8104f197942a4f470754ead699b4d67158
BartonDev <abartondeveloper@gmail.com>
2020-12-05 12:41:33 -0500

    Add dates to weekday cells

--- WorkoutLog/View/Cells/WeekCollectionCell.swift
+++ WorkoutLog/View/Cells/WeekCollectionCell.swift
@@ -19,22 +19,52 @@ class WeekCollectionCell: UICollectionViewCell {
     
     private lazy var weekdayLabel: UILabel = {
         let label = UILabel()
+        label.text = "Wednesday"
+        label.textColor = ColorTheme.lightGray1
+        label.font = UIFont(name: "Avenir Next Condensed", size: 14)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
     private lazy var dateLabel: UILabel = {
         let label = UILabel()
+        label.text = "March 21st"
+        label.textAlignment = .right
+        label.textColor = ColorTheme.lightGray1
+        label.font = UIFont(name: "Avenir Next Condensed", size: 14)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
+    private lazy var dataView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    //TODO: adjust colors
+    //TODO: do we want "March 5th 2020" or "3/5/20" format here? Maybe the latter
+    
     private func setup() {
         self.addSubview(weekdayLabel)
         self.addSubview(dateLabel)
+        self.addSubview(dataView)
         
         NSLayoutConstraint.activate([
-        
+            weekdayLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 5.0),
+            weekdayLabel.trailingAnchor.constraint(equalTo: self.centerXAnchor),
+            weekdayLabel.topAnchor.constraint(equalTo: self.topAnchor),
+            weekdayLabel.heightAnchor.constraint(equalToConstant: 30.0),
+            
+            dateLabel.leadingAnchor.constraint(equalTo: self.centerXAnchor),
+            dateLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -5.0),
+            dateLabel.topAnchor.constraint(equalTo: self.topAnchor),
+            dateLabel.heightAnchor.constraint(equalToConstant: 30.0),
+            
+            dataView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            dataView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            dataView.topAnchor.constraint(equalTo: weekdayLabel.bottomAnchor),
+            dataView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
         ])
         
         self.layer.cornerRadius = 4.0
